6a4b73d36a3620a9bd7c3ab375e8e7a4a172b2c2
Alec Hall <alecnhall@gmail.com>
2019-07-23 17:59:13 +0000

    Automatically backed up by Learn

--- test.rb
+++ test.rb
@@ -1,7 +1,7 @@
 number = [1, 2, 3, 4, 5]
 
 def find_element_index(array, value_to_find)
-  array.length.times{ |value_to_find|
+  array.length.times{ |index|
     puts array[value_to_find]
   }
 end
